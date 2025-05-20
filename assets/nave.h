877479d2c128fae4cb8118b70367f18079f5196e
Matheus Ribeiro <matheusmax140@gmail.com>
2025-05-20 17:30:57 -0300

    inicio

---
+++ assets/nave.h
@@ -0,0 +1,8 @@
+#ifndef NAVE_H
+#define NAVE_H
+
+#include <stdint.h>
+
+extern const uint32_t nave[1][1024];
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,73 @@
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include "assets/nave.h"
+
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+const int bmp_size = 32;
+static SDL_Texture *texture = NULL;
+
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    
+    SDL_IOStream* io = SDL_IOFromConstMem(nave, bmp_size);
+    if (!io) {
+        SDL_Log("Erro criando SDL_IOStream: %s", SDL_GetError());
+    }
+    SDL_Surface* surface = SDL_LoadBMP_IO(io, 1);
+    if (!surface) {
+        SDL_Log("Erro carregando BMP: %s", SDL_GetError());
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+
+    return SDL_APP_CONTINUE;  
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;  
+    }
+    return SDL_APP_CONTINUE; 
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    SDL_FRect dst_rect;
+    dst_rect.x=50;dst_rect.y=50;dst_rect.h=100;dst_rect.w=100;
+    SDL_SetRenderDrawColorFloat(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE); 
+
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); 
+    SDL_RenderDebugText(renderer, 10, 10, "It can be scaled.");
+
+    SDL_RenderClear(renderer);
+    SDL_RenderTexture(renderer, texture, NULL, &dst_rect);
+
+    SDL_RenderPresent(renderer);
+    return SDL_APP_CONTINUE;  
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    /* SDL will clean up the window/renderer for us. */
+}
